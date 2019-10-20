a75b1616b837a79665fae8f2453d5edba25ad506
flokau.bln.dev@gmail.com <flokau.bln.dev@gmail.com>
2019-10-20 21:54:06 +0200

    beautification and reduces some calls

--- lesson1/index.js
+++ lesson1/index.js
@@ -1,4 +1,6 @@
-// first we need some mountains to hike. they will have as many trails and huts, as we create later on. We have our guidebook at hand with getTrails and getHuts
+// first we need some mountains to hike.
+// they will have as many trails and huts, as we create later on.
+// We have our guidebook at hand with getTrails and getHuts
 class Mountain {
   constructor (name) {
     this.name = name
@@ -42,8 +44,6 @@ class Hut {
   }
 
   getTrails () {
-    console.log('------')
-    console.log(this.name, 'has these trails:')
     this.trails.forEach(element => console.log(element.name))
   }
 }
@@ -62,19 +62,19 @@ class Mountaineer {
     console.log('Starting the Trail', trail.name, 'at', this.location.name)
     if (trail.style === 'hike') { // i am pretty sure tere is a nicer way to solve this. this looks very repetitive
       if (this.climbing === true) {
-        console.log('hiking')
+        console.log('Hiking time')
       }
     } else if (trail.style === 'bike') {
       if (this.bike === true) {
-        console.log('going by bike')
+        console.log('Going by bike')
       } else {
         console.log('hiking the trail')
       }
     } else if (trail.style === 'climbing') {
       if (this.climbing === true) {
-        console.log('climbing!')
+        console.log('Climbing!')
       } else {
-        console.log('cannot do that route')
+        console.log('Cannot do that route')
       }
     }
     console.log('Arrived at', trail.endHut.name)
@@ -95,24 +95,23 @@ class Mountaineer {
 
   showOffTrails () {
     console.log('------')
-    console.log(this.name, 'did these trails:')
+    console.log(this.name, 'Has these trails in her all-time trail log:')
     this.log.forEach(element => console.log(element.name))
   }
 }
 
 function doTour (mountaineer) {
-  console.log('------')
-  while (mountaineer.location.name !== 'Home') { // okay I cheated on this one and used knowledge from former courses; this will end up in an infinite loop if the content isn't created accordingly... to be improved
-    console.log(mountaineer.name, 'is currently at', mountaineer.location.name, 'on the mountain', mountaineer.location.mountain.name)
-    mountaineer.location.getTrails()
+  console.log(mountaineer.name, 'is at', mountaineer.location.name, 'on the mountain', mountaineer.location.mountain.name, 'and starting the tour')
+  while (mountaineer.location.name !== 'Home') { // this will end up in an infinite loop if the content isn't created accordingly... to be improved
+    mountaineer.getTrails()
     mountaineer.eat()
     console.log('chosing', mountaineer.location.trails[0].name)
     mountaineer.doTrail(mountaineer.location.trails[0])
   }
-  console.log('Finally arived home. That was quite a Tour!')
+  console.log('>>>> Finally arived home. That was quite a Tour!  <<<<')
 }
 
-// create some content
+// create some content. looking forward to learning how to use a database
 const kickelhahn = new Mountain('Kickelhahn')
 const lindenberg = new Mountain('Lindenberg')
 const goethehuette = new Hut('Goethehuette', kickelhahn, 'Mushroom Omelette')
