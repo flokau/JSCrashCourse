113f2c2bd8a9a44982bbc1fd0c7b7d70a685c535
flokau.bln.dev@gmail.com <flokau.bln.dev@gmail.com>
2019-12-07 14:24:18 +0100

    Dockerized the app

--- mountainTours/mongo-connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-async function main () {
-  mongoose.connect('mongodb://localhost/mountainTours', { useNewUrlParser: true, useUnifiedTopology: true })
-  console.log('connected')
-}
-
-main()
